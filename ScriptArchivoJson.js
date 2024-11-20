16f468acb8e0b6e9798cf5a47a2da766b4a3efc8
Esanpons <ecs200@hotmail.com>
2024-11-20 17:45:34 +0100

    subida!!

--- ScriptArchivoJson.js
+++ ScriptArchivoJson.js
@@ -1,18 +1,20 @@
+var Vanessa = 2025;
+var Aleix = 2025;
+var Pilar = 2025;
+var Mat = 2024;
+var Esteve = 2025;
 var Eventos =
     [
 
         {
             title: 'Aniversari Esteve',
-            start: '2025-01-08',
+            start: Esteve+"01-08",
             mes: 'Setembre',
             year: '2024'
         },
-
-
-
         {
             title: 'Aniversari Vanessa',
-            start: '2025-01-23',
+            start: Vanessa+"01-23",
             mes: 'Setembre',
             year: '2024'
         },
@@ -29,12 +31,6 @@ var Eventos =
             year: '2024'
         },
         {
-            title: 'Aniversari Lois',
-            start: '2025-07-04',
-            mes: 'Setembre',
-            year: '2024'
-        },
-        {
             title: 'Aniversari Aleix',
             start: '2024-09-01',
             mes: 'Setembre',
@@ -42,7 +38,7 @@ var Eventos =
         },
         {
             title: 'Cumple Pilar',
-            start: '2024-10-12',
+            start: +"-",
             mes: 'octubre',
             year: '2024'
         },
@@ -50,12 +46,6 @@ var Eventos =
             title: 'Aniversari Mat',
             start: '2024-12-30',
             mes: 'Desembre',
-            year: '2024'
-        },
-        {
-            title: 'Cumancem Clases',
-            start: '2024-09-09',
-            mes: 'Setembre',
             year: '2024'
         },
     ];
