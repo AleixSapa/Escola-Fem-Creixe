fdd1e1bc7ffeb8cd3859349c5d34e4b35d0899a9
Esanpons <ecs200@hotmail.com>
2024-11-20 11:14:27 +0100

    subida!!

--- Ensenyar a programar/Script.js
+++ Ensenyar a programar/Script.js
@@ -1,24 +1,33 @@
 function Sesio_1_Prova() {
-    alert("Recorda que no pots mirar els exemples de la pàgina només la teva memoria i sobretot google o intel·ligències artificials")
-    alert("Recorda de posar la primera en majúscules.")
-    var prompt01 = prompt("Tenen diferència un h1 que un h2")
+    window.location.href=
+    alert("Recorda que no pots mirar els exemples de la pàgina, només la teva memòria i, sobretot, Google o intel·ligències artificials.");
+    alert("Recorda de posar la primera lletra en majúscula.");
+    var prompt01 = prompt("Tenen diferència un h1 i un h2?");
     if (prompt01 == "Si") {
-        alert("Molt ve o as completat")
+        alert("Molt bé, ho has completat!");
     } else {
-        alert("As Fallat")
-        window.location.href = "https://aleixsapa.github.io/Escola-Fem-Creixe/Ensenyar%20a%20programar/Sesion%201.html"
+        alert("Has fallat.");
+        location.reload()
     }
-    var prompt01 = prompt("")
-    if (prompt01 == "Si") {
-        alert("Molt ve o as completat")
+    var prompt02 = prompt("Hi Que tenen de diferencia");
+    if (prompt02 == "") {
+        alert("Molt bé, ho has completat!");
     } else {
-        alert("As Fallat")
-        window.location.href = "https://aleixsapa.github.io/Escola-Fem-Creixe/Ensenyar%20a%20programar/Sesion%201.html"
+        alert("Has fallat.");
+        location.reload()
     }
-    var prompt01 = prompt("Es Mirat els eggemples o google o intel·ligències artificials")
+    var prompt03 = prompt("");
+    if (prompt03 == "Si") {
+        alert("Molt bé, ho has completat!");
+    } else {
+        alert("Has fallat.");
+        location.reload()
+    }
+
+    var prompt01 = prompt("Has mirat els exemples, Google o intel·ligències artificials?");
     if (prompt01 == "No") {
-        alert("Molt ve o as completat")
+        alert("Ho has fet bé!");
     } else {
-        alert("As Fallat")
+        alert("Molt malament!");
     }
 }
